605670d65a864761791f98e323e08b897506842d
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2015-09-29 22:47:29 +0200

    Added common Assembly Metadata files

---
+++ assignment1/src/impl/Properties/AssemblyInfo.Shared.cs
@@ -0,0 +1,31 @@
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+#region [assembly: AssemblyConfiguration("")]
+#if DEBUG
+#if ANYCPU
+[assembly: AssemblyConfiguration("Debug configuration for any CPU")]
+#elif X86 // !ANYCPU && X86
+[assembly: AssemblyConfiguration("Debug configuration for x86")]
+#elif X64 // !ANYCPU && !X86 && X64
+[assembly: AssemblyConfiguration("Debug configuration for x64")]
+#else // !ANYCPU && !X86 && !X64
+[assembly: AssemblyConfiguration("Debug configuration")]
+#endif
+#else // !DEBUG
+#if ANYCPU
+[assembly: AssemblyConfiguration("Release for any CPU")]
+#elif X86 // !ANYCPU && X86
+[assembly: AssemblyConfiguration("Release for x86")]
+#elif X64 // !ANYCPU && !X86 && X64
+[assembly: AssemblyConfiguration("Release for x64")]
+#else // !ANYCPU && !X86 && !X64
+[assembly: AssemblyConfiguration("Release configuration")]
+#endif
+#endif // !DEBUG
+#endregion
+[assembly: AssemblyCompany("UiT The arctic university of Norway, Department of Computer Science")]
+[assembly: AssemblyCopyright("Written by Tim Alexander Teige, Fredrik Høisæther Rasch, 2015")]
+[assembly: AssemblyTrademark("")]
