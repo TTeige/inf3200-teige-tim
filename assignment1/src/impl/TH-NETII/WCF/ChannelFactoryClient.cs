5ffd87321bd00215162a5ba5d6fb9926a45a4770
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2015-09-29 22:48:49 +0200

    Added TH-NETII WCF support functionality library

---
+++ assignment1/src/impl/TH-NETII/WCF/ChannelFactoryClient.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+
+namespace THNETII.WCF
+{
+	public class ChannelFactoryClient<IService> : ICommunicationObject, IDisposable
+	{
+		private readonly ChannelFactory<IService> factory;
+
+		protected ChannelFactory<IService> ChannelFactory
+		{ get { return factory; } }
+
+		public event EventHandler Closed
+		{
+			add { factory.Closed += value; }
+			remove { factory.Closed -= value; }
+		}
+
+		public event EventHandler Closing
+		{
+			add { factory.Closing += value; }
+			remove { factory.Closing -= value; }
+		}
+
+		public event EventHandler Faulted
+		{
+			add { factory.Faulted += value; }
+			remove { factory.Faulted -= value; }
+		}
+
+		public event EventHandler Opened
+		{
+			add { factory.Opened += value; }
+			remove { factory.Opened -= value; }
+		}
+
+		public event EventHandler Opening
+		{
+			add { factory.Opening += value; }
+			remove { factory.Opening -= value; }
+		}
+
+		public ClientCredentials Credentials => factory.Credentials;
+
+		public ServiceEndpoint Endpoint => factory.Endpoint;
+
+		public CommunicationState State => factory.State;
+
+		protected ChannelFactoryClient(ChannelFactory<IService> factory)
+		{
+			if (factory == null)
+				throw new ArgumentNullException("factory");
+			this.factory = factory;
+		}
+
+		public ChannelFactoryClient() : this(new ChannelFactory<IService>("*")) { }
+		public ChannelFactoryClient(string endpointConfigurationName) : this(new ChannelFactory<IService>(endpointConfigurationName)) { }
+		public ChannelFactoryClient(string endpointConfigurationName, EndpointAddress remoteAddress) : this(new ChannelFactory<IService>(endpointConfigurationName, remoteAddress)) { }
+		public ChannelFactoryClient(Binding binding, EndpointAddress remoteAddress) : this(new ChannelFactory<IService>(binding, remoteAddress)) { }
+
+		public void Abort() => factory.Abort();
+
+		public void Close() => factory.Close();
+
+		public void Close(TimeSpan timeout) => factory.Close(timeout);
+
+		public IAsyncResult BeginClose(AsyncCallback callback, object state) => factory.BeginClose(callback, state);
+
+		public IAsyncResult BeginClose(TimeSpan timeout, AsyncCallback callback, object state) => factory.BeginClose(timeout, callback, state);
+
+		public void EndClose(IAsyncResult result) => factory.EndClose(result);
+
+		public void Open() => factory.Open();
+
+		public void Open(TimeSpan timeout) => factory.Open(timeout);
+
+		public IAsyncResult BeginOpen(AsyncCallback callback, object state) => factory.BeginOpen(callback, state);
+
+		public IAsyncResult BeginOpen(TimeSpan timeout, AsyncCallback callback, object state) => factory.BeginOpen(timeout, callback, state);
+
+		public void EndOpen(IAsyncResult result) => factory.EndOpen(result);
+
+		public void Dispose()
+		{ using (factory) { } }
+	}
+}
---
+++ assignment1/src/impl/TH-NETII/WCF/WebChannelFactoryClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Web;
+
+namespace THNETII.WCF
+{
+	public class WebChannelFactoryClient<IService> : ChannelFactoryClient<IService>
+		where IService : class
+	{
+		protected WebChannelFactory<IService> WebChannelFactory
+		{ get { return ChannelFactory as WebChannelFactory<IService>; } }
+
+		protected WebChannelFactoryClient(WebChannelFactory<IService> factory) : base(factory) { }
+
+		public WebChannelFactoryClient() : base(new WebChannelFactory<IService>("*")) { }
+		public WebChannelFactoryClient(Uri remoteAddress) : base(new WebChannelFactory<IService>(remoteAddress)) { }
+		public WebChannelFactoryClient(string endpointConfigurationName) : base(new WebChannelFactory<IService>(endpointConfigurationName)) { }
+		public WebChannelFactoryClient(string endpointConfigurationName, Uri remoteAddress) : base(new WebChannelFactory<IService>(endpointConfigurationName, remoteAddress)) { }
+		public WebChannelFactoryClient(Binding binding, Uri remoteAddress) : base(new WebChannelFactory<IService>(binding, remoteAddress)) { }
+	}
+}
