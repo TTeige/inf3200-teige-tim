3f0e65919b35d21904dcbe0324088331429001ca
Fredrik Høisæther Rasch <fredrik.h.rasch@uit.no>
2015-10-30 18:14:37 +0100

    Added new empty ASP.NET project

---
+++ assignment2/src/FrontendApp/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Logging;
+
+namespace UiT.Inf3200.FrontendApp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            app.UseDeveloperExceptionPage();
+
+            app.UseMvcWithDefaultRoute();
+
+            app.UseWelcomePage();
+        }
+    }
+}
