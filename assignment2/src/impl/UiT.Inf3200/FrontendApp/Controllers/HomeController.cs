596084bb495de017b2a874ad0d15aa1cfbacbbf9
Fredrik Høisæther Rasch <fredrik.h.rasch@uit.no>
2015-11-02 22:39:03 +0100

    Changed ASP.NET template to Yeoman

--- assignment2/src/impl/UiT.Inf3200/FrontendApp/Controllers/HomeController.cs
+++ assignment2/src/impl/UiT.Inf3200/FrontendApp/Controllers/HomeController.cs
@@ -1,19 +1,35 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
-
-namespace UiT.Inf3200.FrontendApp.Controllers
+namespace no.uit.inf3200_2015_2.assignment2.fra006_tte008.FrontendApp.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View("~/Views/Shared/Error.cshtml");
         }
     }
 }
