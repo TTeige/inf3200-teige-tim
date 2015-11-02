823aea093cd7de2312db060fffeb1aa5026c971e
Fredrik Høisæther Rasch <fredrik.h.rasch@uit.no>
2015-11-02 16:38:49 +0100

    Added Empty MVC controller/views

--- assignment2/src/impl/UiT.Inf3200/FrontendApp/Startup.cs
+++ assignment2/src/impl/UiT.Inf3200/FrontendApp/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
 using Microsoft.Framework.DependencyInjection;
 using Microsoft.Framework.Logging;
 
@@ -8,15 +9,27 @@ namespace UiT.Inf3200.FrontendApp
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add MVC services to the services container.
             services.AddMvc();
+
+            // Uncomment the following line to add Web API services which makes it easier to port Web API 2 controllers.
+            // You will also need to add the Microsoft.AspNet.Mvc.WebApiCompatShim package to the 'dependencies' section of project.json.
+            // services.AddWebApiConventions();
         }
 
-        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        // Configure is called after ConfigureServices is called.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
 
+            // Configure the HTTP request pipeline.
+            app.UseBrowserLink();
             app.UseDeveloperExceptionPage();
 
+            // Add static files to the request pipeline.
+            app.UseStaticFiles();
+
+            // Add MVC to the request pipeline.
             app.UseMvcWithDefaultRoute();
 
             app.UseWelcomePage();
