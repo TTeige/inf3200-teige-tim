a77f52b8fa0780c4e22fb8669bf9765c5b397adc
Fredrik Høisæther Rasch <fredrik.h.rasch@uit.no>
2015-11-08 22:07:37 +0100

    Added functionality to modify size of available nodes list

--- assignment2/startup.sh
+++ assignment2/startup.sh
@@ -7,7 +7,7 @@ p2pNodeExe="src/impl/UiT.Inf3200/P2PNode/bin/Debug/P2PNode.exe"
 rm -fv $availableNodesFile
 touch $availableNodesFile
 
-for nd in `sh /share/apps/bin/available-nodes.sh`
+for nd in `cat hostfile`
 do
 	echo Starting node on $nd
 	nohup ssh $nd "cd \"$directory\" ; nohup mono $p2pNodeExe > $nd.p2p.out 2>&1 & " > /dev/null 2>&1 &
