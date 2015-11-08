75be054c643f36b48a3478cb39cb3b1a91fc9899
Fredrik Høisæther Rasch <fredrik.h.rasch@uit.no>
2015-11-08 17:52:55 +0100

    Added P2P Node without leader election

---
+++ assignment2/src/impl/UiT.Inf3200/P2PNode/Program.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mime;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace P2PNode
+{
+    static class Program
+    {
+        private static readonly ReaderWriterLockSlim nodeStateLock = new ReaderWriterLockSlim();
+        private static ManualResetEvent terminateSignal = new ManualResetEvent(initialState: false);
+        private static Guid nodeGuid;
+        private static int portNumber;
+
+        private static readonly ConcurrentDictionary<Guid, Tuple<string, int>> outgoingNodeConnections = new ConcurrentDictionary<Guid, Tuple<string, int>>();
+        private static readonly ConcurrentDictionary<Guid, Tuple<string, int>> incomingNodeConnections = new ConcurrentDictionary<Guid, Tuple<string, int>>();
+
+        static void Main(string[] args)
+        {
+            THNETII.AssemblySplash.WriteAssemblySplash();
+
+            Console.WriteLine();
+
+            if (args == null || args.Length < 1 || !int.TryParse(args[0], out portNumber))
+                portNumber = 8899;
+
+            Uri frontEndAddr = null;
+            if (args != null && args.Length > 1 && !string.IsNullOrWhiteSpace(args[1]))
+                frontEndAddr = new Uri(args[1]);
+
+            string host = "+";
+            if (args != null && args.Length > 2)
+                host = args[2];
+
+            nodeGuid = Guid.NewGuid();
+
+            Console.CancelKeyPress += OnTerminateSignal;
+
+            var http_listener = new HttpListener();
+            http_listener.Prefixes.Add(string.Format("http://{0}:{1}/", host, portNumber));
+
+            http_listener.Start();
+            HandleHttpContext(http_listener);
+
+            if (frontEndAddr != null)
+            {
+                var uriBuilder = new UriBuilder(new Uri(frontEndAddr, "/Nodes/Register"));
+
+                var client = new WebClient { BaseAddress = frontEndAddr.ToString() };
+                client.UploadValuesTaskAsync("Nodes/Register", new System.Collections.Specialized.NameValueCollection
+                {
+                    { "guid", nodeGuid.ToString() }
+                });
+            }
+
+            terminateSignal.WaitOne();
+
+            //nodeStateLock.EnterWriteLock();
+            try
+            {
+                var connectedNodes = outgoingNodeConnections.ToArray().Concat(incomingNodeConnections.ToArray());
+                var deregistrationTaskList = new List<Task>();
+                foreach (var otherNodeKvp in connectedNodes)
+                {
+                    deregistrationTaskList.Add(DeregisterNodeAtNeighbour(otherNodeKvp.Value.Item1, otherNodeKvp.Value.Item2));
+                }
+
+                Task.WaitAll(deregistrationTaskList.ToArray());
+
+                incomingNodeConnections.Clear();
+                outgoingNodeConnections.Clear();
+
+                http_listener.Stop();
+            }
+            finally
+            {
+                //nodeStateLock.ExitWriteLock();
+            }
+        }
+
+        private static async Task DeregisterNodeAtNeighbour(string hostname, int port)
+        {
+            var uriBuilder = new UriBuilder(Uri.UriSchemeHttp, hostname, port, "/deregisterNode");
+            var req = WebRequest.Create(uriBuilder.Uri) as HttpWebRequest;
+            req.Method = WebRequestMethods.Http.Post;
+            var contentTypeObj = new ContentType(MediaTypeNames.Text.Plain);
+            contentTypeObj.CharSet = Encoding.ASCII.WebName;
+            req.ContentType = contentTypeObj.ToString();
+
+            var reqBodyData = Encoding.ASCII.GetBytes(nodeGuid.ToString());
+            req.ContentLength = reqBodyData.LongLength;
+
+            using (var reqStream = await req.GetRequestStreamAsync())
+            {
+                await reqStream.WriteAsync(reqBodyData, 0, reqBodyData.Length);
+                await reqStream.FlushAsync();
+            }
+
+            using (var resp = await req.GetResponseAsync()) { }
+        }
+
+        private static void OnTerminateSignal(object sender, ConsoleCancelEventArgs e)
+        {
+            if (e.SpecialKey == ConsoleSpecialKey.ControlC)
+            {
+                e.Cancel = terminateSignal.Set();
+            }
+        }
+
+        private static async void HandleHttpContext(HttpListener http_listener)
+        {
+            HttpListenerContext http_ctx;
+            try { http_ctx = await http_listener.GetContextAsync(); }
+            catch (Exception) { return; }
+
+            //nodeStateLock.EnterReadLock();
+
+            try
+            {
+                HandleHttpContext(http_listener);
+
+                var path = http_ctx.Request.Url.AbsolutePath;
+                if (string.Equals("/getCurrentLeader", path, StringComparison.OrdinalIgnoreCase))
+                {
+                    http_ctx.Response.ContentType = MediaTypeNames.Text.Plain;
+                    http_ctx.Response.ContentEncoding = Encoding.ASCII;
+                    http_ctx.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                    http_ctx.Response.Close(Encoding.ASCII.GetBytes("The requested resource could not be found."), willBlock: false);
+                }
+                else if (string.Equals("/getNodes", path, StringComparison.OrdinalIgnoreCase))
+                {
+                    GetNodesContext(http_ctx);
+                }
+                else if (string.Equals("/connectToNodes", path, StringComparison.OrdinalIgnoreCase))
+                {
+                    await ConnectToNodesContext(http_ctx);
+                }
+                else if (string.Equals("/registerNeighbour", path, StringComparison.OrdinalIgnoreCase))
+                {
+                    RegisterNeighbourContext(http_ctx);
+                }
+                else if (string.Equals("/deregisterNode", path, StringComparison.OrdinalIgnoreCase))
+                {
+                    await DeregisterNodeContext(http_ctx);
+                }
+                else
+                {
+                    http_ctx.Response.ContentType = MediaTypeNames.Text.Plain;
+                    http_ctx.Response.ContentEncoding = Encoding.ASCII;
+                    http_ctx.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                    http_ctx.Response.Close(Encoding.ASCII.GetBytes("The requested resource could not be found."), willBlock: false);
+                }
+            }
+            finally
+            {
+                //nodeStateLock.ExitReadLock();
+            }
+        }
+
+        private static void GetNodesContext(HttpListenerContext http_ctx)
+        {
+            http_ctx.Response.StatusCode = (int)HttpStatusCode.OK;
+            http_ctx.Response.ContentType = MediaTypeNames.Text.Plain;
+            http_ctx.Response.ContentEncoding = Encoding.ASCII;
+            using (var respWriter = new StreamWriter(http_ctx.Response.OutputStream))
+            {
+                foreach (var connectedNodeKvp in outgoingNodeConnections.ToArray().Concat(incomingNodeConnections.ToArray()))
+                {
+                    respWriter.WriteLine(string.Format("{0}:{1}", connectedNodeKvp.Value.Item1, connectedNodeKvp.Value.Item2));
+                }
+                respWriter.Flush();
+            }
+            http_ctx.Response.Close();
+        }
+
+        private static async Task DeregisterNodeContext(HttpListenerContext http_ctx)
+        {
+            var reqEncoding = http_ctx.Request.ContentEncoding ?? Encoding.ASCII;
+            Guid otherGuid;
+            using (var reqReader = new StreamReader(http_ctx.Request.InputStream, reqEncoding))
+                otherGuid = Guid.Parse(await reqReader.ReadLineAsync());
+            Tuple<string, int> ignoreTuple;
+            outgoingNodeConnections.TryRemove(otherGuid, out ignoreTuple);
+            incomingNodeConnections.TryRemove(otherGuid, out ignoreTuple);
+
+            http_ctx.Response.StatusCode = (int)HttpStatusCode.OK;
+            http_ctx.Response.Close(new byte[0], willBlock: false);
+        }
+
+        private static async Task ConnectToNodesContext(HttpListenerContext http_ctx)
+        {
+            var contentTypeObj = new ContentType(MediaTypeNames.Text.Plain);
+            contentTypeObj.CharSet = Encoding.ASCII.WebName;
+            var contentTypeString = contentTypeObj.ToString();
+            var reqBodyData = Encoding.ASCII.GetBytes(GetNodeSpecString(http_ctx));
+
+            var registrationTaksList = new List<Task>();
+            using (var bodyReader = new StreamReader(http_ctx.Request.InputStream, http_ctx.Request.ContentEncoding))
+            {
+                var bodyLineTask = bodyReader.ReadLineAsync();
+                for (var bodyLine = await bodyLineTask; bodyLine != null; bodyLine = await bodyLineTask)
+                {
+                    bodyLineTask = bodyReader.ReadLineAsync();
+
+                    var nodeAddr = bodyLine;
+                    var colIdx = nodeAddr.LastIndexOf(':');
+                    UriBuilder uriBuilder;
+                    if (colIdx < 0)
+                        uriBuilder = new UriBuilder(Uri.UriSchemeHttp, nodeAddr);
+                    else
+                        uriBuilder = new UriBuilder(Uri.UriSchemeHttp, nodeAddr.Substring(0, colIdx), int.Parse(nodeAddr.Substring(colIdx + 1)));
+
+                    registrationTaksList.Add(ExchangeWithNode(uriBuilder.Uri, reqBodyData, contentTypeString));
+                }
+            }
+
+            await Task.WhenAll(registrationTaksList);
+            http_ctx.Response.StatusCode = (int)HttpStatusCode.OK;
+            http_ctx.Response.Close(new byte[0], willBlock: false);
+        }
+
+        private static async Task ExchangeWithNode(Uri otherNodeUri, byte[] nodeSpecData, string contentType)
+        {
+            var req = WebRequest.Create(new Uri(otherNodeUri, "/registerNeighbour")) as HttpWebRequest;
+            req.Method = WebRequestMethods.Http.Post;
+            req.ContentType = contentType;
+            req.ContentLength = nodeSpecData.Length;
+            using (var reqStream = await req.GetRequestStreamAsync())
+            {
+                await reqStream.WriteAsync(nodeSpecData, 0, nodeSpecData.Length);
+                await reqStream.FlushAsync();
+            }
+
+            Guid otherGuid; string otherHostname; int otherPort;
+            using (var resp = await req.GetResponseAsync() as HttpWebResponse)
+            {
+                var respEncoding = string.IsNullOrWhiteSpace(resp.ContentEncoding) ? Encoding.ASCII : Encoding.GetEncoding(resp.ContentEncoding);
+                using (var respReader = new StreamReader(resp.GetResponseStream(), respEncoding))
+                {
+                    otherGuid = Guid.Parse(await respReader.ReadLineAsync());
+                    otherHostname = await respReader.ReadLineAsync();
+                    otherPort = int.Parse(await respReader.ReadLineAsync());
+                }
+
+            }
+
+            if (nodeGuid.CompareTo(otherGuid) < 0)
+                outgoingNodeConnections[otherGuid] = Tuple.Create(otherHostname, otherPort);
+            else
+                incomingNodeConnections[otherGuid] = Tuple.Create(otherHostname, otherPort);
+        }
+
+        private static void RegisterNeighbourContext(HttpListenerContext http_ctx)
+        {
+            Guid otherGuid; string otherHostname; int otherPort;
+            using (var bodyReader = new StreamReader(http_ctx.Request.InputStream, http_ctx.Request.ContentEncoding ?? Encoding.ASCII))
+            {
+                otherGuid = Guid.Parse(bodyReader.ReadLine());
+                otherHostname = bodyReader.ReadLine();
+                otherPort = int.Parse(bodyReader.ReadLine());
+            }
+
+            if (nodeGuid.CompareTo(otherGuid) < 0)
+                outgoingNodeConnections[otherGuid] = Tuple.Create(otherHostname, otherPort);
+            else
+                incomingNodeConnections[otherGuid] = Tuple.Create(otherHostname, otherPort);
+
+            var respBodyString = GetNodeSpecString(http_ctx);
+            var repsBodyData = Encoding.ASCII.GetBytes(respBodyString);
+            http_ctx.Response.StatusCode = (int)HttpStatusCode.OK;
+            http_ctx.Response.ContentType = MediaTypeNames.Text.Plain;
+            http_ctx.Response.ContentEncoding = Encoding.ASCII;
+            http_ctx.Response.Close(repsBodyData, willBlock: false);
+        }
+
+        private static string GetNodeSpecString(HttpListenerContext http_ctx)
+        {
+            var nodeSpecBuilder = new StringBuilder();
+            nodeSpecBuilder
+                .AppendLine(nodeGuid.ToString())
+                .AppendLine(http_ctx.Request.LocalEndPoint.Address.ToString())
+                .Append(http_ctx.Request.LocalEndPoint.Port);
+            return nodeSpecBuilder.ToString();
+        }
+    }
+}
---
+++ assignment2/src/impl/UiT.Inf3200/P2PNode/Properties/AssemblyInfo.cs
@@ -0,0 +1,57 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("P2PNode")]
+[assembly: AssemblyDescription("")]
+#region [assembly: AssemblyConfiguration("")]
+#if DEBUG
+#if ANYCPU
+[assembly: AssemblyConfiguration("Debug configuration for any CPU")]
+#elif X86 // !ANYCPU && X86
+[assembly: AssemblyConfiguration("Debug configuration for x86")]
+#elif X64 // !ANYCPU && !X86 && X64
+[assembly: AssemblyConfiguration("Debug configuration for x64")]
+#else // !ANYCPU && !X86 && !X64
+[assembly: AssemblyConfiguration("Debug configuration")]
+#endif
+#else // !DEBUG
+#if ANYCPU
+[assembly: AssemblyConfiguration("Release for any CPU")]
+#elif X86 // !ANYCPU && X86
+[assembly: AssemblyConfiguration("Release for x86")]
+#elif X64 // !ANYCPU && !X86 && X64
+[assembly: AssemblyConfiguration("Release for x64")]
+#else // !ANYCPU && !X86 && !X64
+[assembly: AssemblyConfiguration("Release configuration")]
+#endif
+#endif // !DEBUG
+#endregion
+[assembly: AssemblyCompany("UiT The arctic university of Norway, Department of Computer Science")]
+[assembly: AssemblyCopyright("Written by Tim Alexander Teige, Fredrik Høisæther Rasch, 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyProduct("Inf-3200 - Assignment 2")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("640d747c-b71c-4a36-8f44-5d468badf8ca")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.120.*")]
+[assembly: AssemblyFileVersion("2.0.120")]
